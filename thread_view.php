0cbf78e601cd2719e03f8e9db302d46e00a85df8
Brit <britkos@gmail.com>
2015-04-11 14:50:57 -0600

    fixed a bug in thread_view

--- thread_view.php
+++ thread_view.php
@@ -1,47 +1,49 @@
-<?php
-// get the php thread id
-$id = 0;
-if(array_key_exists("id", $_GET)) {
-    $id = $_GET["id"];
-}
-
-
-
-
-// get the database
-$db;
-$db_obj = new database();
-$db = $db_obj->get_db();
-
-
-// prep select
-$sql = "SELECT * FROM threads WHERE id=?";
-if( !($stmt = $db->prepare($sql)) ) { echo "Prepare failed: (" . $db->errno . ") " . $db->error; }
-
-// bind vals to sql
-if( !$stmt->bind_param("i", $id) ) { echo "Binding parameters failed in login: (" . $stmt->errno . ") " . $stmt->error; }
-
-// exe stmt
-if( !$stmt->execute() ) { echo "Execute failed in login: (" . $stmt->errno . ") " . $stmt->error; }
-
-// get result
-if( !($res = $stmt->get_result()) ) { echo "Getting result set failed in login: (" . $stmt->errno . ") " . $stmt->error; }
-$thread = $res->fetch_assoc();
-$stmt->close();
-
-// test to make sure the db data was pulled correctly
-//echo '<pre>' . print_r($thread, true) . '</pre>';die();
-
-
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
 
     <title>Poast | Thread View</title>
     <?php include "file_include.php" ?>
+
+
+    <?php
+    // get the php thread id
+    $id = 0;
+    if(array_key_exists("id", $_GET)) {
+        $id = $_GET["id"];
+    }
+
+
+
+
+    // get the database
+    $db;
+    $db_obj = new database();
+    $db = $db_obj->get_db();
+
+
+    // prep select
+    $sql = "SELECT * FROM threads WHERE id=?";
+    if( !($stmt = $db->prepare($sql)) ) { echo "Prepare failed: (" . $db->errno . ") " . $db->error; }
+
+    // bind vals to sql
+    if( !$stmt->bind_param("i", $id) ) { echo "Binding parameters failed in login: (" . $stmt->errno . ") " . $stmt->error; }
+
+    // exe stmt
+    if( !$stmt->execute() ) { echo "Execute failed in login: (" . $stmt->errno . ") " . $stmt->error; }
+
+    // get result
+    if( !($res = $stmt->get_result()) ) { echo "Getting result set failed in login: (" . $stmt->errno . ") " . $stmt->error; }
+    $thread = $res->fetch_assoc();
+    $stmt->close();
+
+    // test to make sure the db data was pulled correctly
+    //echo '<pre>' . print_r($thread, true) . '</pre>';die();
+
+
+    ?>
+
+    
 
 </head>
 <body>
