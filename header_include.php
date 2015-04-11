51aeeb980b07686f1f588f28ee4b2b2c433c7d7b
Brett Bloethner <Brett@BrettBloethner.com>
2015-04-11 14:22:29 -0600

    test

--- header_include.php
+++ header_include.php
@@ -2,8 +2,8 @@
 <header class="header-container">
     <div class="header">
 
-        <a href="new_thread.php" class="new-thread-button"
-           title="create a thread">create a thread</a>
+       <!-- <a href="new_thread.php" class="new-thread-button"
+           title="create a thread">create a thread</a> -->
 
         <a href="index.php" class="logo">poast.io</a>
 
