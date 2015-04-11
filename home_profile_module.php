ab338b2ea1b1a761c8dbc2afd7ed19622d25d528
Brett Bloethner <Brett@BrettBloethner.com>
2015-04-11 17:14:15 -0600

    test

---
+++ home_profile_module.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brett Bloethner
+ * Date: 4/11/2015
+ * Time: 5:13 PM
+ */
--- index.php
+++ index.php
@@ -13,11 +13,14 @@
 
     <section id="home" class="container12">
         <div class="thirtypadding"></div>
-        <div class="column7" id="static_thread">
+        <div class="column6" profile" id="profile">
+            <?php include "home_profile_module.php" ?>
+        </div>
+        <div class="column6 staticfeed" id="static_thread">
             <?php include "partial_static_thread.php" ?>
         </div>
         <br>
-        <div class="column6" id="generated_thread">
+        <div class="column9 dynamicfeed" id="generated_thread">
             <?php include "partial_generated_threads.php" ?>
         </div>
 
