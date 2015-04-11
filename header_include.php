20e3ab2e61600f7052e81fa279ee06ce210cd246
Brett Bloethner <Brett@BrettBloethner.com>
2015-04-11 09:31:37 -0600

    test

--- header_include.php
+++ header_include.php
@@ -2,7 +2,7 @@
 <header class="header-container">
     <div class="header">
 
-        <a href="/new_thread.php" class="new-thread-button"
+        <a href="new_thread.php" class="new-thread-button"
            title="create a thread">create a thread</a>
 
         <p class="logo"> poast.io</p>
--- new_thread.php
+++ new_thread.php
@@ -10,12 +10,28 @@
 
     <?php include "header_include.php" ?>
 
-
-
     <section id="new_thread">
 
+    <div class="container12">
+
+            <div class="column12">
+
+                <div class="new-thread-box">
+
+                    <form class="new-thread-form">
+                        <h1 class="subtitle">
+                            Thread Title
+                        </h1>
+                        <input type="text" name="firstname">
+                        <br>
+                        Thread Content<br>
+                        <input type="text" name="lastname">
+                    </form>
+
+                </div>
+
+            </div>
+    </div>
     </section>
-
-
 </body>
 </html>
