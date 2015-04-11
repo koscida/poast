24f97d19f12eb5096c96c90fb72e99ce9c2e66f8
Brit <britkos@gmail.com>
2015-04-11 12:22:03 -0600

    finished (for now) user generated posts

--- partial_generated_threads.php
+++ partial_generated_threads.php
@@ -49,20 +49,22 @@ function time_ago ($oldTime, $timeType = "h") {
 foreach($all_posts as $key => $thread) {
 ?>
     <div class="thread column12 outer">
+        <a class="thread_link" href="thread_view.php?id=<?php echo $thread['id']; ?>">
 
-        <h2><a href""><?php echo $thread['title']; ?></a></h2>
-        <div class="date"><?php echo time_ago($thread['create_date']); ?></div>
+            <span class="title"><?php echo $thread['title']; ?></span> &nbsp;
+            <span class="date"><?php echo time_ago($thread['create_date']); ?></span>
 
-        <div class="clear"></div>
+            <span class="clear"></span>
 
-        <div class="location"><?php echo $thread['created_lat']; ?></div>
+            <span class="location"><?php echo $thread['created_lat']; ?></span>
 
-        <div class="score">
-            <span class="score_toasts"></span>
-            <span class="score_num"><?php echo ($thread['num_toasts'] - $thread['num_roasts']); ?></span>
-            <span class="score_roasts"></span>
-        </div>
+            <span class="score">
+                <span class="score_toasts"></span>
+                <span class="score_num"><?php echo ($thread['num_toasts'] - $thread['num_roasts']); ?></span>
+                <span class="score_roasts"></span>
+            </span>
 
+        </a>
     </div>
 
 
--- thread_view.php
+++ thread_view.php
@@ -11,7 +11,7 @@
     <?php include "header_include.php" ?>
 
     <section id="thread_view">
-
+        
     </section>
 
 </body>
