8e41b0bf8d5fa446fef79524538b86966e873fdb
David <dabo9011@colorado.edu>
2015-04-11 13:34:23 -0600

    formating

--- new_thread.php
+++ new_thread.php
@@ -26,12 +26,12 @@
 
                     <form class="new-thread-form">
                         <h1 class="subtitle">
-                            Title
+                            title
                         </h1>
                         <input class="new-thread-title-field" type="text" name="firstname">
                         <br><br>
                         <h1 class="subtitle">
-                            Message
+                            message
                         </h1>
                         <textarea class="new-thread-text-area" rows="4" cols="50"></textarea>
 
