add4a614c7051f9610a298776a511d1b3e8f8250
Brett Bloethner <Brett@BrettBloethner.com>
2015-04-11 10:24:01 -0600

    test

--- new_thread.php
+++ new_thread.php
@@ -18,14 +18,25 @@
 
                 <div class="new-thread-box">
 
+                    <p class="new-thread-page-title">
+                    Start a new thread
+
+                    </p>
+
                     <form class="new-thread-form">
                         <h1 class="subtitle">
-                            Thread Title
+                            thread title
                         </h1>
-                        <input type="text" name="firstname">
-                        <br>
-                        Thread Content<br>
-                        <input type="text" name="lastname">
+                        <input class="new-thread-title-field" type="text" name="firstname">
+                        <br><br>
+                        <h1 class="subtitle">
+                            thread text
+                        </h1>
+                        <textarea class="new-thread-text-area" rows="4" cols="50">
+
+                        </textarea>
+
+                        <input class="new-thread-button new-thread-create-button" type="submit" value="create">
                     </form>
 
                 </div>
