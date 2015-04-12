e81d9182d84991d94779da5cd9db6373d476cfdd
Brit <britkos@gmail.com>
2015-04-12 12:30:11 -0600

    added local/hyper local buttons

--- partial_generated_threads.php
+++ partial_generated_threads.php
@@ -3,15 +3,18 @@
 
         //prep_ajax();
         $("#local").click(function(){
-            prep_ajax(0.003);
+            prep_ajax(0.005);
+            $("#h").html(0.005);
         });
         $("#hyper").click(function(){
             prep_ajax(0.001);
+            $("#h").html(0.001);
         });
 
         function prep_ajax(loc) {
             if ("geolocation" in navigator) {
-                navigator.geolocation.getCurrentPosition(function (position, loc) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    var loc = $("#h").html();
                     do_ajax(position.coords.latitude, position.coords.longitude, loc);
                 });
             } else { }
@@ -20,6 +23,7 @@
 
         function do_ajax(lat, long, loc) {
             console.log("in do ajax");
+            console.log(loc);
             var data = {
                 lat: lat,
                 long: long,
@@ -40,7 +44,7 @@
     });
 </script>
 
-<div id="h" style="display: none">0</div>
+<div id="h" style="display: none">0.001</div>
 
 <div class="threads_header column12 outer">
     <p>Local Toasts: <span>Boulder</span></p>
