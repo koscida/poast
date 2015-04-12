dd92b31a1c4f38b5e760d686d1843cfd8aaa77b4
Brit <britkos@gmail.com>
2015-04-11 22:33:11 -0600

    FINALLY HAVE THE POSTS PAGE WORKING!!! FUCK YEAH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- helper_functions.php
+++ helper_functions.php
@@ -1,16 +1,85 @@
 <?php
+function print_array($a) {
+    echo '<pre>'.print_r($a, true).'</pre>';
+}
+
+
 /*
 * ***********************************************************************
-* Function to get how long ago the post was
+* PHP Function to get how long ago the post was
 */
 function time_ago ($oldTime, $timeType = "h") {
-$newTime = date('Y-m-d h:i:s', time());
-$timeType = "h";
+    $newTime = date('Y-m-d h:i:s', time());
+    $timeType = "h";
 
-$timeCalc = strtotime($newTime) - strtotime($oldTime);
-$timeCalc = round($timeCalc/60/60) . " hours ago";
-return $timeCalc;
+    $timeCalc = strtotime($newTime) - strtotime($oldTime);
+    $timeCalc = round($timeCalc/60/60) . " hours ago";
+    return $timeCalc;
 }
+/*
+* ***********************************************************************
+*/
+
+
+
+
+
+/*
+* ***********************************************************************
+*  PHP function to sort and organize the posts (replies)
+*/
+
+function organize_posts($posts) {
+    //echo "in organize_posts";
+    $org_posts = array();
+
+    foreach($posts as $key => $post) {
+        $reply_id = $post['post_reply_id'];
+        //echo "<br/><br/>" . $post['id'] . "<br/>";
+
+        // first test if it is top level reply
+        if($reply_id == 0) {
+            $org_posts[$post['id']] = array("post" => $post, "replies" => array());
+        // else it is a reply to a post
+        } else {
+            $op = recursive_check_array($post, $org_posts);
+            $org_posts = $op[1];
+            //print_array($org_posts);
+        }
+    }
+
+    return $org_posts;
+}
+
+function recursive_check_array($post, $org_posts) {
+    //echo "in recursive<br/>";
+    $reply_id = $post['post_reply_id'];
+
+    if(array_key_exists($reply_id, $org_posts)) {
+        $org_posts[$reply_id]['replies'][$post['id']] = array("post" => $post, "replies" => array());
+        return array(true, $org_posts);
+    } else {
+        reset($org_posts);
+        //echo "count: " . count($org_posts) . "<br/>";
+        for($i=0; $i<count($org_posts); $i++) {
+            //echo "i: " . $i . "<br/>";
+            $p = each($org_posts);
+            $key_id = $p['key'];
+            //echo "key_id: " . $key_id . "<br/>";
+
+            //echo "key_id replies is not 0<br/>";
+            $op = recursive_check_array($post, $org_posts[$key_id]['replies']);
+            // true - found a match
+            if($op[0]) {
+                $org_posts[$key_id]['replies'] = $op[1];
+                return array(true, $org_posts);
+                // false - did not find a match
+            }
+        }
+
+    }
+}
+
 /*
 * ***********************************************************************
 */
--- thread_view.php
+++ thread_view.php
@@ -22,6 +22,9 @@
     $db = $db_obj->get_db();
 
 
+    /*
+     * Get the Data for the Thread
+     */
     // prep select
     $sql = "SELECT * FROM threads WHERE id=?";
     if( !($stmt = $db->prepare($sql)) ) { echo "Prepare failed: (" . $db->errno . ") " . $db->error; }
@@ -40,7 +43,72 @@
     // test to make sure the db data was pulled correctly
     //echo '<pre>' . print_r($thread, true) . '</pre>';die();
 
+
+    /*
+     * Get the Data for the Thread's Posts
+     */
+    // prep select
+    $sql = "SELECT p.*, u.username AS username FROM posts AS p
+        LEFT JOIN users AS u ON u.id = p.author_id
+        WHERE p.thread_id = ?";
+    if( !($stmt = $db->prepare($sql)) ) { echo "Prepare failed: (" . $db->errno . ") " . $db->error; }
+
+    // bind vals to sql
+    if( !$stmt->bind_param("i", $id) ) { echo "Binding parameters failed in login: (" . $stmt->errno . ") " . $stmt->error; }
+
+    // exe stmt
+    if( !$stmt->execute() ) { echo "Execute failed in login: (" . $stmt->errno . ") " . $stmt->error; }
+
+    // get result
+    $posts = array();
+    if( !($res = $stmt->get_result()) ) { echo "Getting result set failed in login: (" . $stmt->errno . ") " . $stmt->error; }
+    while ($row = $res->fetch_assoc()) {
+        $posts[$row['id']] = $row;
+    }
+    $stmt->close();
+
+    // test to make sure the db data was pulled correctly
+    //print_array($posts); //die();
+
+    $organized_posts = organize_posts($posts);
+    //print_array($organized_posts); die();
+
+
     $img =  empty($thread['image_path']) ? false : true;
+
+
+
+    function print_post($p) {
+        ?>
+        <div class="single_post">
+            <div class="expand_button active">+</div>
+            <div class="expand_content">
+                <div class="votes">
+                    <div class="score_toasts"></div>
+                    <div class="score_roasts"></div>
+                </div>
+                <div class="post_content">
+                    <div class="post_text"><?php echo $p['text']; ?></div>
+                    <span class="post_author"><?php echo $p['username']; ?></span>
+                    <span class="post_score"><?php echo $p['num_toasts'] - $p['num_roasts']; ?></span>
+                    <span class="post_reply"><a href="#">Reply</a></span>
+                 </div>
+            </div>
+        </div>
+        <?php
+    }
+
+    function recursive_print_posts($post, $replies) {
+        echo '<div class="post_group">';
+        print_post($post);
+        if(!empty($replies)) {
+            foreach($replies as $k => $p)
+                recursive_print_posts($p['post'], $p['replies']);
+        }
+        echo '</div>';
+    }
+
+
     ?>
 
 
@@ -89,7 +157,11 @@
             </div>
 
             <div class="posts">
-
+                <?php
+                foreach($organized_posts as $key => $p) {
+                    recursive_print_posts($p['post'], $p['replies']);
+                }
+                ?>
 
             </div>
 
