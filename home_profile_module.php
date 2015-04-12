6e4b1fbf07873b28d91ed70c9007eaee820f899c
Brett Bloethner <Brett@BrettBloethner.com>
2015-04-11 22:54:00 -0600

    test

--- home_profile_module.php
+++ home_profile_module.php
@@ -3,23 +3,7 @@
 </div>
 
 <div class=" column12 outer profilepane">
-    <table class="profile-table" border="1" style="width:75%">
-        <tr>
-            <td>POSTS</td>
-            <td>SWEG</td>
-        </tr>
-        <tr>
-            <td>TOASTS</td>
-            <td>420</td>
-        </tr>
-            <td>ROASTS</td>
-            <td>69</td>
-        </tr>
-        <tr>
-            <td>MEMBER SINCE:</td>
-            <td>yes</td>
-        </tr>
-    </table>
+
     <a href="new_thread.php" class="new-thread-button"
        title="create a thread">create a thread</a>
     </div>
